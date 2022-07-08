901c39ee3233e4967f09e606313ddf05bd0833b6
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-07-08 13:52:32 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -2,8 +2,22 @@ Blockly.Blocks['mq_get_ppm'] = {
     init: function() {
       this.jsonInit(
         {
-          "message0": "đọc giá trị nồng độ ppm chân %1",
+          "message0": "đọc giá trị %1 cảm biến chân %2",
           "args0": [
+            {
+                "type": "field_dropdown",
+                "name": "PARAM",
+                "option": [
+                    [
+                        "ppm",
+                        "PPM"
+                    ],
+                    [
+                        "mg/l",
+                        "MG/L"
+                    ]
+                ]
+            },
             {
               "type": "field_dropdown",
               "name": "NAME",
@@ -37,61 +51,18 @@ Blockly.Blocks['mq_get_ppm'] = {
   
   Blockly.Python['mq_get_ppm'] = function(block) {
     var dropdown_name = block.getFieldValue('NAME');
+    var dropdown_name = block.getFieldValue('PARAM');
     Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
     Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin';
     Blockly.Python.definitions_['import_mq'] = 'from mq import MQ';
     Blockly.Python.definitions_["import_create_mq"] = 'mq = MQ(Pin(' + dropdown_name + '.adc_pin)) # analog PIN';
     // TODO: Assemble Python into code variable.
-    var code = 'mq.get_ppm()';
+    var code = '';
+    if (dropdown_data == "PPM")
+      code = "mq.get_ppm()";
+    else
+      code = "mq.get_acohol()";
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, Blockly.Python.ORDER_NONE];
   };
 
-  Blockly.Blocks['mq_get_acohol'] = {
-    init: function() {
-      this.jsonInit(
-        {
-          "message0": "đọc giá trị nồng độ cồn chân %1",
-          "args0": [
-            {
-              "type": "field_dropdown",
-              "name": "NAME",
-              "options": [
-                [
-                  "P0",
-                  "pin0"
-                ],
-                [
-                  "P1",
-                  "pin1"
-                ],
-                [
-                  "P2",
-                  "pin2"
-                ]
-              ]
-            }
-          ],
-          "output": null,
-          "colour": "#6642bf",
-          "helpUrl": ""
-        }
-      );
-    },
-    getDeveloperVars: function() {
-      return ['mq3'];
-    }
-    
-  };
-  
-  Blockly.Python['mq_get_acohol'] = function(block) {
-    var dropdown_name = block.getFieldValue('NAME');
-    Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
-    Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin';
-    Blockly.Python.definitions_['import_mq'] = 'from mq import MQ';
-    Blockly.Python.definitions_["import_create_mq"] = 'mq = MQ(Pin(' + dropdown_name + '.adc_pin)) # analog PIN';
-    // TODO: Assemble Python into code variable.
-    var code = "'{:.3f}'.format(mq.get_acohol())";
-    // TODO: Change ORDER_NONE to the correct strength.
-    return [code, Blockly.Python.ORDER_NONE];
-  };
